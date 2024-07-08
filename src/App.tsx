cc84f30614fd5fdfb7b31ba0dc5e3a20e09f1322
adrian <adrian.vide.paris@gmail.com>
2024-07-08 23:15:06 +0200

    Implement infinite scroll with Intersection Observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Worker } from "./types/worker";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "./store/store";
@@ -9,25 +9,70 @@ function App() {
   const dispatch = useDispatch<AppDispatch>();
   const workers = useSelector((state: RootState) => state.workers.workers);
   const workerStatus = useSelector((state: RootState) => state.workers.status);
-  const error = useSelector((state: RootState) => state.workers.error);
+  const [page, setPage] = useState(1);
+
+  const lastWorkerRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    if (workerStatus === "idle") {
-      dispatch(fetchWorkers(1));
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && workerStatus !== "loading") {
+          dispatch(fetchWorkers(page + 1));
+          setPage(page + 1);
+        }
+      });
+    }, options);
+
+    if (lastWorkerRef.current) {
+      observer.observe(lastWorkerRef.current);
     }
-  }, [workerStatus, dispatch]);
+
+    return () => {
+      if (lastWorkerRef.current) {
+        observer.unobserve(lastWorkerRef.current);
+      }
+    };
+  }, [dispatch, page, workerStatus]);
+  useEffect(() => {
+    if (workerStatus !== "loading") {
+      console.log("Fetching workers...");
+      dispatch(fetchWorkers(page));
+    }
+  }, []);
+
+  const WorkerComponent = ({ worker }: { worker: Worker }) => (
+    <div>
+      <img
+        src="https://placehold.co/400x300"
+        alt={`${worker.first_name} ${worker.last_name}`}
+      />
+      <h2>
+        {worker.first_name} {worker.last_name}
+      </h2>
+      <p>{worker.profession}</p>
+    </div>
+  );
+
   return (
     <div className="App">
-      <div className="App">
-        {workers?.map((worker: Worker) => (
-          <div key={worker.id}>
-            <img src="https://placehold.co/400x300" alt={worker.first_name} />
-            <h2>
-              {worker.first_name} {worker.last_name}
-            </h2>
-            <p>{worker.profession}</p>
-          </div>
-        ))}
-      </div>
+      {workers?.map((worker: Worker, index: number) => {
+        if (index === workers.length - 1) {
+          return (
+            <div ref={lastWorkerRef} key={worker.id}>
+              LAST
+              <WorkerComponent worker={worker} />
+            </div>
+          );
+        } else {
+          return <WorkerComponent key={worker.id} worker={worker} />;
+        }
+      })}
     </div>
   );
 }
